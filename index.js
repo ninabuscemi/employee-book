09da1433cf7da4d7d0d7f45083984b5123c14449
Nina B <ngbuscemi@gmail.com>
2024-02-25 01:51:09 -0500

    added starter code for application

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require('inquirer');
 const mysql = require("mysql12");
 const cTable = require("console.table");
 
+// Creates a connection to MySQL database
+
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -18,5 +20,52 @@ db.connect((err) => {
 
 // Function to start the application
 function startApp() {
-    
+    inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+            "Exit",
+        ],
+        },
+    ])
+
+    .then((answer) => {
+        switch (answer.action) {
+            case "View all departments":
+                viewDepartments();
+                break;
+            case "View all roles":
+                viewRoles();
+                break;
+            case "View all employees":
+                viewEmployees();
+                break;
+            case "Add a department":
+                addDepartment();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "Add an employee":
+                addEmployee();
+                break;
+            case "Update an employee role":
+                updateEmployeeRole();
+                break;
+            case "Exit":
+                console.log("Exiting the application.");
+                db.end();
+                break;
+        }
+    });
 }
