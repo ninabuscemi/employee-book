f3718cb6a2757e4bd214de75ac834f4560be1268
Nina B <ngbuscemi@gmail.com>
2024-02-25 02:19:03 -0500

    added function to update employee role

--- index.js
+++ index.js
@@ -206,3 +206,48 @@ function updateEmployeeRole() {
     });
   });
 }
+
+// Function to update an employee's role
+function updateEmployeeRole() {
+  // Query to get employee names and role titles for choices
+  db.query("SELECT * FROM employees", (err, employees) => {
+    if (err) throw err;
+
+    db.query("SELECT * FROM roles", (err, roles) => {
+      if (err) throw err;
+
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "employeeId",
+            message: "Select the employee to update:",
+            choices: employees.map((employee) => ({
+              name: `${employee.first_name} ${employee.last_name}`,
+              value: employee.employee_id,
+            })),
+          },
+          {
+            type: "list",
+            name: "roleId",
+            message: "Select the employee's new role:",
+            choices: roles.map((role) => ({
+              name: role.title,
+              value: role.role_id,
+            })),
+          },
+        ])
+        .then((answer) => {
+          db.query(
+            "UPDATE employees SET role_id = ? WHERE employee_id = ?",
+            [answer.roleId, answer.employeeId],
+            (err, results) => {
+              if (err) throw err;
+              console.log("Employee role updated successfully.");
+              startApp();
+            }
+          );
+        });
+    });
+  });
+}
